09ee9be7eee7df7de5110aff38bb8e3e5362fede
pabbises <sivakumar.pabbisetty@cdk.com>
2023-11-10 13:03:43 +0530

    resolve the bug

--- react_code/src/App.js
+++ react_code/src/App.js
@@ -1,9 +1,8 @@
-
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 
 const MapComponent = () => {
-  const [cities, setCities] = useState([]);
+  const [citiesData, setCitiesData] = useState([]);
   const [selectedCities, setSelectedCities] = useState([]);
   const [paths, setPaths] = useState([]);
 
@@ -11,46 +10,52 @@ const MapComponent = () => {
     fetch('https://gist.githubusercontent.com/dastagirkhan/00a6f6e32425e0944241/raw/33ca4e2b19695b2b93f490848314268ed5519894/gistfile1.json')
       .then(response => response.json())
       .then(data => {
-        setCities(data);
+        setCitiesData(data);
       })
       .catch(error => console.error('Error fetching data:', error));
   }, []);
 
   const handleCitySelection = (city) => {
-    setSelectedCities(prevCities => {
-      if (prevCities.includes(city)) {
-        return prevCities.filter(selectedCity => selectedCity !== city);
+    setSelectedCities(prevSelectedCities => {
+      if (prevSelectedCities.includes(city.name)) {
+        return prevSelectedCities.filter(selectedCity => selectedCity !== city.name);
       } else {
-        return [...prevCities, city];
+        return [...prevSelectedCities, city.name];
       }
     });
   };
 
   const findPaths = () => {
-    const selectedCityCoordinates = selectedCities.map(city => ({
-      lat: parseFloat(city.lat),
-      lon: parseFloat(city.lon),
-    }));
+    const selectedCityCoordinates = selectedCities.map(cityName => {
+      const city = citiesData.find(city => city.name === cityName);
+      return {
+        lat: parseFloat(city.lat),
+        lon: parseFloat(city.lon),
+      };
+    });
     const paths = [selectedCityCoordinates];
     setPaths(paths);
   };
 
   const findShortestPath = () => {
-    const selectedCityCoordinates = selectedCities.map(city => ({
-      lat: parseFloat(city.lat),
-      lon: parseFloat(city.lon),
-    }));
+    const selectedCityCoordinates = selectedCities.map(cityName => {
+      const city = citiesData.find(city => city.name === cityName);
+      return {
+        lat: parseFloat(city.lat),
+        lon: parseFloat(city.lon),
+      };
+    });
     const paths = [selectedCityCoordinates];
     fetch('http://localhost:3001/api/shortest-path', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ paths }),
-  })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error sending shortest path:', error));
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ paths }),
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error sending shortest path:', error));
 
     setPaths(paths);
   };
@@ -60,12 +65,12 @@ const MapComponent = () => {
       <div>
         <h2>Available Cities</h2>
         <ul>
-          {cities.map((city, index) => (
-            <li key={city.name} id = {index}>
+          {citiesData.map((city, index) => (
+            <li key={city.name} id={index}>
               <label>
                 <input
                   type="checkbox"
-                  checked={selectedCities.includes(city)}
+                  checked={selectedCities.includes(city.name)}
                   onChange={() => handleCitySelection(city)}
                 />
                 {city.name}
@@ -81,14 +86,17 @@ const MapComponent = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        {selectedCities.map(city => (
-          <Marker
-            key={city.name}
-            position={[parseFloat(city.lat), parseFloat(city.lon)]}
-          >
-            <Popup>{city.name}</Popup>
-          </Marker>
-        ))}
+        {selectedCities.map(cityName => {
+          const city = citiesData.find(city => city.name === cityName);
+          return (
+            <Marker
+              key={city.name}
+              position={[parseFloat(city.lat), parseFloat(city.lon)]}
+            >
+              <Popup>{city.name}</Popup>
+            </Marker>
+          );
+        })}
         {paths.map((path, index) => (
           <Polyline
             key={index}
